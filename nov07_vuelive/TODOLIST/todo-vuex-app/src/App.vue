2e50b9b10195ae557a73a4d7b2cda5ab76d6fc71
giokim12 <giokim1011@gmail.com>
2022-11-07 17:44:55 +0900

    .

---
+++ nov07_vuelive/TODOLIST/todo-vuex-app/src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="app">
+    <h1>TODO LIST</h1>
+    <h2>ALL TODOS: {{ allTodosCount }}</h2>
+    <h2>COMPLETED TODOS: {{ completedTodosCount }}</h2>
+    <h2>INCOMPLETE TODOS: {{ incompleteTodosCount }}</h2>
+    <TodoList/>
+    <Todoform/>
+    <!-- 데이터 다시 불러오는 버튼 만들기 -->
+    <!-- <button @click='loadTodos'>할일 불러오기</button> -->
+  </div>
+</template>
+
+<script>
+import TodoList from '@/components/TodoList'
+import Todoform from '@/components/Todoform'
+
+export default {
+  name: 'App',
+  components: {
+    TodoList,
+    Todoform,
+  },
+  methods: {
+    loadTodos() {
+        this.$store.dispatch('loadTodos')
+    },
+  },
+  computed: {
+    allTodosCount() {
+        return this.$store.getters.allTodosCount
+    },
+    completedTodosCount() {
+        return this.$store.getters.completedTodosCount
+    },
+    incompleteTodosCount() {
+        return this.$store.getters.incompleteTodosCount
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ nov07_vuelive/vuex-app/src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="app">
+    <h1> {{ message }}</h1>
+    <h2>입력된 문자의 길이는 {{ messageLength }}</h2>
+    <input 
+      type="text"
+      @keyup.enter = "changeMessage"
+      v-model="inputData"
+    >
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  </div>
+</template>
+
+<script>
+// import { computed } from 'vue'
+// import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      inputData: null,
+    }
+    // HelloWorld
+  },
+  computed: {
+    message() {
+        return this.$store.state.message
+    },
+    messageLength() {
+      return this.$store.getters.messageLength
+    },
+  },
+  methods: {
+    changeMessage() {
+      const newMessage = this.inputData
+      // this.$store.dispatch('액션 메서드 이름', 새 메시지)
+      this.$store.dispatch('changeMessage', newMessage)
+    }
+
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
